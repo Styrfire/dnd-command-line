ae6c7f3e02d4eabf51c7c106e3eefaf427841578
Styrfire <g_n_rogers1991@yahoo.com>
2019-10-18 17:19:51 -0500

    WIP Setup Character functionality

--- src/main/groovy/com/dnd/util/SetupCharacter.java
+++ src/main/groovy/com/dnd/util/SetupCharacter.java
@@ -2,17 +2,17 @@ package com.dnd.util;
 
 import com.dnd.dto.playerCharacter.PlayerCharacter;
 import com.dnd.dto.playerCharacter.Fighter;
-import com.dnd.dto.weapon.Greatsword;
 import com.dnd.dto.weapon.Longsword;
 import com.dnd.enums.Race;
 
 public class SetupCharacter
 {
-	public static PlayerCharacter setupCharacterByClass(String playerClass)
+	public static PlayerCharacter setupDefaultCharacterByClass(String playerClass)
 	{
 		PlayerCharacter playerCharacter = new PlayerCharacter();
 
-		switch (playerClass.toLowerCase()) {
+		switch (playerClass.toLowerCase())
+		{
 			case "fighter":
 
 				playerCharacter.setName("Tuuk-Tuuk");
@@ -43,8 +43,65 @@ public class SetupCharacter
 		return playerCharacter;
 	}
 
-	public static PlayerCharacter setUpCharacter()
+	// use http://chicken-dinner.com/5e/5e-point-buy.html
+	// custom rules:
+	// Total Pointes = 31
+	// Max Purchasable Attribute Before Bonuses: 17
+	public static PlayerCharacter setupcharacter()
 	{
+		PlayerCharacter playerCharacter;
+		System.out.println(
+				"Greetings Adventurer! First thing's first, what is your name?\n\n" +
+						"Type in your character's name and press enter."
+		);
+
+		String name = Input.readInput();
+		while (name.equals(""))
+		{
+			System.out.println(
+					"I'm sorry, that is not a valid name...\n\n" +
+							"Type in your character's name and press enter."
+			);
+
+			name = Input.readInput();
+		}
+
+		playerCharacter = new PlayerCharacter();
+		playerCharacter.setName(name);
+
+		System.out.println(
+				name + "? Ah, yes! That is a fine name. Now what race is your adventurer?\n\n" +
+						"Your choices are:\n" +
+						"\thuman\n\n" +
+						"Type in one of the above choices and press enter."
+		);
+
+		playerCharacter.setRace(selectRace(Input.readInput()));
+		while (playerCharacter.getRace() == null)
+		{
+			System.out.println(
+					"I'm sorry, that is not a valid race...\n\n" +
+							"Type in your character's race and press enter."
+			);
+
+			playerCharacter.setRace(selectRace(Input.readInput()));
+		}
+
+		System.out.println("Character isn't fully created yet!!! Prepare for a null pointer exception!");
 		return null;
+	}
+
+
+	private static Race selectRace(String race)
+	{
+		switch(race.toLowerCase())
+		{
+			case "human":
+				return Race.HUMAN;
+			case "elf":
+				return Race.ELF;
+			default:
+				return null;
+		}
 	}
 }
